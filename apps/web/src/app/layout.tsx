b6baf50dc0f03c4d21ac1b12b3707d809543e76e
Gabriel Vallés <hernandezvallesgabriel@gmail.com>
2024-10-19 16:15:58 -0400

    fix(apps/web): metadataBase corregida

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -22,7 +22,7 @@ export function generateMetadata(): Metadata {
   return {
     title,
     description,
-    metadataBase: new URL(`https://${process.env.CF_PAGES_URL}`),
+    metadataBase: new URL(`${process.env.CF_PAGES_URL}`),
     openGraph: {
       title,
       description,
